be097fa381730cd387ce93530eeb0e8a8d9b1b3b
Thomas Alfonso <thomalfonso@gmail.com>
2019-06-24 21:57:43 -0400

    save encripted file in server

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,67 @@
 import React, { Component } from 'react';
+import { Button } from 'reactstrap';
 
 import logo from './logo.svg';
-
 import './App.css';
+let aes256 = require('aes256');
 
 class App extends Component {
   state = {
     response: '',
     post: '',
-    responseToPost: '',
+    responseToPost: ''
   };
-  
+
   componentDidMount() {
     this.callApi()
       .then(res => this.setState({ response: res.express }))
       .catch(err => console.log(err));
   }
+  clickInLoad = () => {
+    document.getElementById("inputUpload").click();
+    let UpdFile = document.getElementById("inputUpload");
+    UpdFile.addEventListener('change',this.handleFileSelect, false);
+  }
   
+  handleFileChosen = (file) => {
+    let fileReader = new FileReader();
+    fileReader.onloadend = function (e){
+      var content = fileReader.result;
+
+      /* encrypt */
+      var key = "my passphrase;"
+      var contentEncrypted = aes256.encrypt(key, content);
+      var nameEncrypted = aes256.encrypt(key, file.name);
+      //nameEncrypted = nameEncrypted.toString().replace(":", "\/");
+      //
+      /* create file encrypt */
+      var fileEncrypted = new File([contentEncrypted], nameEncrypted);
+      console.log("fileEncrypted",fileEncrypted);
+      var formData = new FormData();
+      //formData.set('files', files);
+      formData.append('files', fileEncrypted);
+      fetch('api/file', { // Your POST endpoint
+        method: 'POST',
+        body: formData // This is your file object
+      })
+      .catch(function() {
+        console.log("error in post data");
+      });
+    }
+    fileReader.readAsText(file);
+  }
+
+  handleFileSelect = (evt) => {
+    if(evt.target.files.length>0){
+      var files = evt.target.files; // FileList object
+
+      // Loop through the FileList and render image files as thumbnails.
+      for (var i = 0;i<files.length; i++) {
+        this.handleFileChosen(files[i])
+      }
+    }
+  }
+
   callApi = async () => {
     const response = await fetch('/api/hello');
     const body = await response.json();
@@ -69,6 +114,10 @@ render() {
           <button type="submit">Submit</button>
         </form>
         <p>{this.state.responseToPost}</p>
+
+      {/* file */}
+      <Button onClick={this.clickInLoad} color="primary">primary</Button>{' '}
+      <input type="file" id="inputUpload" name="files[]" multiple/> 
       </div>
     );
   }
--- server.js
+++ server.js
@@ -11,11 +11,42 @@ app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
 
+app.get('/', function(req, res){
+  res.sendFile('index.html');
+}); 
+
+app.get('/api/getFile', function(req, res){
+  res.sendFile(basepath + '/gabythom/DOtO4PB4TU9Jrg9f5ekSut1sqrvfmz3YZtPpGp:R');
+}); 
+
 app.post('/api/world', (req, res) => {
   console.log(req.body);
   res.send(
     `I received your POST request. This is what you sent me: ${req.body.post}`,
   );
+});
+
+const multer = require("multer");
+var basepath = process.cwd();
+
+const multerConfig = {
+    
+    storage: multer.diskStorage({
+     //Setup where the user's file will go
+     destination: function(req, files, next){
+       next(null,basepath + '/gabythom');
+       },   
+    
+        //Then give the file a unique name
+        filename: function(req, files, next){
+        //console.log(files);
+        next(null, files.originalname);
+          }
+        })   
+}
+
+app.post('/api/file',multer(multerConfig).array('files',2),function(req,res){   
+    res.send('Complete!');
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
