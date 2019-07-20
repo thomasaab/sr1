29ac5a9b3ca4ce7ff130a5cce540cdbfb8ea5c0d
Thomas Alfonso <thomalfonso@gmail.com>
2019-07-20 13:51:28 -0400

    mejorado el encriptado

--- client/src/App.js
+++ client/src/App.js
@@ -13,9 +13,7 @@ class App extends Component {
   };
 
   componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
+
   }
   clickInLoad = () => {
     document.getElementById("inputUpload").click();
@@ -26,11 +24,28 @@ class App extends Component {
   handleFileChosen = (file) => {
     let fileReader = new FileReader();
     fileReader.onloadend = function (e){
+      console.log("file",file);
+     
       var content = fileReader.result;
+      console.log("result",content)
+      // let view = new Int8Array(content);
+      // let contentString = '';
+      // for(var i=0;i<view.length;i++){
+      //   if(i==0){
+      //     contentString = contentString.concat(view[i].toString())
+      //   }else{
+      //     contentString = contentString.concat(" " + view[i].toString())
+      //   }   
+      // }
 
+      // console.log(view);
+
+      // console.log("content",contentString)
+      console.log("fileReader",fileReader)
       /* encrypt */
       var key = "my passphrase;"
-      var contentEncrypted = aes256.encrypt(key, content);
+      // var contentEncrypted = aes256.encrypt(key, contentString);
+      var contentEncrypted = aes256.encrypt(key, content)
       var nameEncrypted = aes256.encrypt(key, file.name);
       //nameEncrypted = nameEncrypted.toString().replace(":", "\/");
       //
@@ -48,33 +63,137 @@ class App extends Component {
         console.log("error in post data");
       });
     }
-    fileReader.readAsText(file);
+    fileReader.readAsBinaryString(file);
+    //fileReader.readAsArrayBuffer(file);
+    //codificar a base 64 
   }
 
    callFile = async () => {
     const response = await fetch('/api/getFile');
     const body = await response.blob();
     if (response.status !== 200) throw Error(body.message);
-    
-    console.log(body);
-
+    let key = "my passphrase;"
     let fileReader = new FileReader();
+ 
     fileReader.onloadend = function (e){
-      var content = fileReader.result;
-      console.log("content", content);
+
+      let content = aes256.decrypt(key,fileReader.result);
+      console.log("contentdes", content);
+      var fileDown =[];
+      var filename = "pruba.pdf";
+      fileDown[0]=content;
+      fileDown[1]=filename;
+      
+      fetch('api/postForDownload', { // Your POST endpoint
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ "file": {
+          "content" : content,
+          "name" : filename
+        }}) // This is your file object
+        }).catch(function() {
+          console.log("error in post data");
+        });
+     // download(content,filename,"js");
+     
+      function download(data, filename, type) {
+        var file = new Blob([data], {type: type});
+        if (window.navigator.msSaveOrOpenBlob) // IE10+
+            window.navigator.msSaveOrOpenBlob(file, filename);
+        else { // Others
+            var a = document.createElement("a"),
+                    url = URL.createObjectURL(file);
+            a.href = url;
+            a.download = filename;
+            document.body.appendChild(a);
+            a.click();
+            setTimeout(function() {
+                document.body.removeChild(a);
+                window.URL.revokeObjectURL(url);  
+            }, 0); 
+        }
+    
+      }
+
     }
     fileReader.readAsText(body);
   };
+
+  download = (filename, text) => {
+    var element = document.createElement('a');
+    element.setAttribute('href', encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
 
   handleFileSelect = (evt) => {
     if(evt.target.files.length>0){
       var files = evt.target.files; // FileList object
 
       // Loop through the FileList and render image files as thumbnails.
-      for (var i = 0;i<files.length; i++) {
-        this.handleFileChosen(files[i])
-      }
+      // for (var i = 0;i<files.length; i++) {
+      //   this.handleFileChosen(files[i])
+      // }
+      console.log("llamo")
+      this.postContentFile(files);
+      
     }
+  }
+  postContentFile = async (files) =>{
+    let flagError=false;
+    
+      var formData = new FormData();
+      //formData.set('files', files);
+      console.log("entro", formData)
+      formData.append('files', files[0]);
+      fetch('api/postContentFile', { // Your POST endpoint
+      method: 'POST',
+      body: formData // This is your file object
+      }).catch(function() {
+        console.log("error in post data");
+        flagError=true;
+      });
+    
+    this.getContentFile(files,flagError);
+  }
+
+  getContentFile = async (files,flagError) =>{
+    const response = await fetch('api/getContentFile?name='+files[0].name).catch(function() {
+			console.log("error in get files");
+			flagError=true;
+			});
+			if(!flagError){
+			let json = await response.json();
+
+				setTimeout(() => {
+          var filename = files[0].name;
+          console.log("json",json)
+
+          var key = "my passphrase;"
+          // var contentEncrypted = aes256.encrypt(key, contentString);
+          var contentEncrypted = aes256.encrypt(key, json)
+          var nameEncrypted = aes256.encrypt(key, filename);
+          //nameEncrypted = nameEncrypted.toString().replace(":", "\/");
+          //
+          /* create file encrypt */
+          var fileEncrypted = new File([contentEncrypted], nameEncrypted);
+          console.log("fileEncrypted",fileEncrypted);
+          var formData = new FormData();
+          //formData.set('files', files);
+          formData.append('files', fileEncrypted);
+          fetch('api/file', { // Your POST endpoint
+            method: 'POST',
+            body: formData // This is your file object
+          })
+
+				}, 500);
+			}
   }
 
   callApi = async () => {
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 5000;
+const multer = require('multer');
+const basepath = process.cwd();
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
@@ -16,7 +18,7 @@ app.get('/', function(req, res){
 }); 
 
 app.get('/api/getFile', function(req, res){
-  res.sendFile(basepath + '/gabythom/DOtO4PB4TU9Jrg9f5ekSut1sqrvfmz3YZtPpGp:R');
+  res.sendFile(basepath + '/gabythom/mEe2L4AWDzlyLeVyWrhv9dnVZ8PPygqmIg==');
 }); 
 
 app.post('/api/world', (req, res) => {
@@ -25,11 +27,65 @@ app.post('/api/world', (req, res) => {
     `I received your POST request. This is what you sent me: ${req.body.post}`,
   );
 });
-
-const multer = require("multer");
-var basepath = process.cwd();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.post('/api/postForDownload', (req, res) => {
+  //console.log(req.body.file.name)
+  //console.log(req.body.file.content.replace(/\s/g, ''))
+  var buf = Buffer.from(req.body.file.content.replace(/\s/g, ''), "hex")
+  //console.log(buf)
+  fs.writeFile('mynewfile3.jpg', buf, function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  }); 
+  fs.close;
+});
 
 const multerConfig = {
+    
+	storage: multer.diskStorage({
+	 //Setup where the user's file will go
+	 destination: function(req, files, next){
+	   next(null,basepath + '/temporal');
+	 },   
+	
+		//Then give the file a unique name
+		filename: function(req, files, next){
+		//console.log(files);
+		next(null, files.originalname);
+		  }
+		})   
+}
+
+app.post('/api/postContentFile',multer(multerConfig).array('files',2),function(req,res){	
+  res.send('Complete!');
+  console.log("complete")
+ });
+
+ app.get('/api/getContentFile', function (req, res) {
+   var name = req.query.name;
+ console.log("name",name)
+  fs.readFile(basepath + '/temporal/'+name, function read(err, data) {
+    if (err) {
+      console.log(err);
+    }
+     console.log("data",data);
+     var dataHexa = data.toString('hex').match(/../g).join(' ');
+     console.log("dataHexa",dataHexa);
+     var arrayHexa = dataHexa.split(" ");
+     fs.close;
+   
+    // fs.unlink(basepath + '/temporal/'+name, (err) => {
+    //   if (err) throw err;
+    //     console.log('Deleted succesfully');
+    // }); 
+      fs.close;
+      res.json(dataHexa);
+
+   });
+ })
+
+const multerConfig2 = {
     
     storage: multer.diskStorage({
      //Setup where the user's file will go
@@ -45,7 +101,7 @@ const multerConfig = {
         })   
 }
 
-app.post('/api/file',multer(multerConfig).array('files',2),function(req,res){   
+app.post('/api/file',multer(multerConfig2).array('files',2),function(req,res){   
     res.send('Complete!');
 });
 
