a4525cd433e485c200d958ee8813c891d98da8b9
Thomas Alfonso <thomalfonso@gmail.com>
2019-08-01 17:39:40 -0400

    crreate folder and befor done

--- client/src/File.js
+++ client/src/File.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import filePic from "./file.svg";
 import { Container, Row, Col } from 'reactstrap';
+import menuPic from "./menu.svg";
 
 class File extends Component {
    
     
   render() {
       return (
-        <div className="clickeable element" >
+        <div className="element" >
+          <Row>
+            <img className="menuPic"  src={menuPic} width="25px" alt="Folder"></img>
+          </Row>
+          <div className="clickeable">
             <Row>
-                <img className="fileIcon" src={filePic} width="80px" alt="File"></img>
+              <img className="fileIcon" src={filePic} width="80px" alt="File"></img>
             </Row>
             <Row >
               <p className="textElement" width="60px">{this.props.labelFile}</p>
             </Row>
-           
+          </div>
         </div>
       );
     }
--- client/src/Folder.js
+++ client/src/Folder.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import folderPic from "./folder.svg";
+import menuPic from "./menu.svg";
 import { Container, Row, Col } from 'reactstrap';
 
 class Folder extends Component {
@@ -9,13 +10,26 @@ class Folder extends Component {
   }
   render() {
       return (
-        <div className="clickeable element" onClick={this.clickCard}>
+        <div className=" element" onClick={this.clickCard}>
+          <Row>
+            <img className="menuPic"  src={menuPic} width="25px" alt="Folder"></img>
+            <div class="dropdown-menu ddm1">	
+              <label for="toggle">Menu</label>
+              <ul>
+                <li><a href="#" title="Do something">Do something</a></li>
+                <li><a href="#" title="Do something else">Do something else</a></li>
+                <li><a href="#" title="Options">Options</a></li>
+              </ul>
+            </div>
+          </Row>
+          <div className="clickeable">
             <Row>
-                <img className="folderIcon" src={folderPic} width="80px" alt="Folder"></img>
+              <img className="folderIcon" src={folderPic} width="80px" alt="Folder"></img>
             </Row>
             <Row>
-                <p className="textElement" width="60px">{this.props.labelFile}</p>
+              <p className="textElement" width="60px">{this.props.labelFile}</p>
             </Row>
+          </div>
         </div>
       );
     }
